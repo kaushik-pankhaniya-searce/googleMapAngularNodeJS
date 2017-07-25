684e9a87fcbac437fa2db7ee24718e517071ffde
Kaushik Pankhaniya <kaushik.pankhaniya@punesez.searce.local>
2017-07-25 12:02:49 +0530

    port change to 27017

--- Angular_node_eg/app.js
+++ Angular_node_eg/app.js
@@ -13,7 +13,7 @@ var mongo = require('mongodb');
 //console.log(mongo);
 var monk = require('monk');
 //console.log(monk);
-var db = monk('localhost:27217/googleMapData');
+var db = monk('localhost:27017/googleMapData');
 //console.log(db);
 
 var app = express();
